e3170fd1eaeae4e4db81226a0d0d12175eda5c28
ITworkonline <1130172267@qq.com>
2022-10-21 14:31:42 -0600

    Add files via upload

---
+++ rawtojpg.py
@@ -0,0 +1,25 @@
+import sys
+import os
+import rawpy
+import imageio
+
+image_folder = sys.argv[0]
+output_folder = sys.argv[1]
+
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+for file in os.listdir(image_folder):
+    if file == ".DS_Store":
+        continue
+    raw = rawpy.imread(f'{image_folder}/{file}')
+    thumb = raw.extract_thumb()
+    base_name = os.path.splitext(file)[0]
+    print(base_name)
+    if thumb.format == rawpy.ThumbFormat.JPEG:
+        f = open(f'{output_folder}/{base_name}.jpeg', 'wb')
+        f.write(thumb.data)
+        print(f'{base_name} DONE')
+    elif thumb.format == rawpy.ThumbFormat.BITMAP:
+        imageio.imsave(f'{output_folder}/{base_name}.jpeg', thumb.data)
+        print(f'{base_name} DONE')
